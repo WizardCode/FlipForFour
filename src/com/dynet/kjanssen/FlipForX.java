4806d8e2ae0f7eb80710810a759eacaad89430f7
Kyle Janssen <janssen@seawolf.sonoma.edu>
2014-02-26 14:57:40 -0800

    The command-line version of the game works now.

--- src/com/dynet/kjanssen/FlipForX.java
+++ src/com/dynet/kjanssen/FlipForX.java
@@ -1,4 +1,119 @@
 package com.dynet.kjanssen;
 
 public class FlipForX {
+
+    int size;
+    int toWin;
+    char [][] positions;
+    int [] counts;
+
+    public FlipForX (int S, int W)
+    {
+        size = S;
+        toWin = W;
+
+        positions = new char[size + 2][size + 2];
+        for (int i = 0; i < size + 2; i++)
+            for (int j = 0; j < size + 2; j++)
+                positions[i][j] = ' ';
+
+        counts = new int[size + 2];
+        for (int i = 0; i < size + 2; i++)
+            counts[i] = 0;
+    }
+
+    public String toString ()
+    {
+        String outs = "\n";
+
+        for (int j = 0; j < size; j++)
+            outs += "+-------";
+        outs += "+\n";
+        for (int i = size; i > 0; i--)
+        {
+            for (int j = 0; j < size; j++)
+                outs += "|       ";
+            outs += "|\n";
+            for (int j = 1; j <= size; j++)
+                outs += "|   " + positions[i][j] + "   ";
+            outs += "|\n";
+            for (int j = 0; j < size; j++)
+                outs += "|       ";
+            outs += "|\n";
+            for (int j = 0; j < size; j++)
+                outs += "+-------";
+            outs += "+\n";
+        }
+        for (int j = 0; j < size; j++)
+            outs += "    " + (j+1) + "   ";
+        outs += "\n";
+
+        return outs;
+    }
+
+    public boolean Play (int col, char play, int who)
+    {
+        if (play == 'D' && counts[col] < size) {
+            counts[col]++;
+            positions[counts[col]][col] = Character.toChars(who + 48)[0];
+            return true;
+        } else if (play == 'F' && counts[col] > 0) {
+            for (int i = 1, j = counts[col]; i < j; i++, j--) {
+                char temp = positions[i][col];
+                positions[i][col] = positions[j][col];
+                positions[j][col] = temp;
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    public int Test ()
+    {
+        int first = 1, last = size - toWin + 1;
+        for (int r = 1; r <= size; r++)
+            for (int c = first; c <= last; c++)
+                if (positions[r][c] != ' ')
+                {
+                    int count = 1;
+                    for (int i = c+1; positions[r][i] == positions[r][c]; i++)
+                        count++;
+                    if (count >= toWin)
+                        return positions[r][c] - '0';
+                }
+        for (int c = 1; c <= size; c++)
+            for (int r = first; r <= last; r++)
+                if (positions[r][c] != ' ')
+                {
+                    int count = 1;
+                    for (int i = r+1; positions[i][c] == positions[r][c]; i++)
+                        count++;
+                    if (count >= toWin)
+                        return positions[r][c] - '0';
+                }
+        for (int r = first; r <= last; r++)
+        {
+            for (int c = first; c <= last; c++)
+                if (positions[r][c] != ' ')
+                {
+                    int count = 1;
+                    for (int i = r+1, j = c+1; positions[i][j] == positions[r][c]; i++, j++)
+                        count++;
+                    if (count >= toWin)
+                        return positions[r][c] - '0';
+                }
+            for (int c = size; c >= toWin; c--)
+                if (positions[r][c] != ' ')
+                {
+                    int count = 1;
+                    for (int i = r+1, j = c-1; positions[i][j] == positions[r][c]; i++, j--)
+                        count++;
+                    if (count >= toWin)
+                        return positions[r][c] - '0';
+                }
+        }
+        return 0;
+    }
+
 }
--- src/com/dynet/kjanssen/Main.java
+++ src/com/dynet/kjanssen/Main.java
@@ -1,8 +1,67 @@
 package com.dynet.kjanssen;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int SIZE = 5;
+        int WIN = 4;
+        FlipForX FFX = new FlipForX(SIZE, WIN);
+        int player = 1;
+        int winner = 0;
+        boolean done = false;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(FFX);
+        while (!done && winner == 0)
+        {
+            String colStr, playStr;
+            int column = 0;
+            char play = ' ';
+            do
+            {
+                System.out.print("Player " + player + ", enter the column for your play: ");
+                colStr = in.next();
+                //System.out.println(colStr);
+                if (colStr.charAt(0) == 'q' || colStr.charAt(0) == 'Q')
+                    done = true;
+                else
+                    column = Integer.parseInt(colStr);
+            } while (!done && (column < 1 || column > SIZE));
+            if (done)
+                break;
+            do
+            {
+                System.out.print("Enter the type of play - F for flip - D for drop: ");
+                playStr = in.next();
+                //System.out.println(playStr);
+                if (playStr.charAt(0) == 'q' || playStr.charAt(0) == 'Q')
+                    done = true;
+                else
+                    play = playStr.toUpperCase().charAt(0);
+            } while (!done && play != 'F' && play != 'D');
+            if (done)
+                break;
+
+            // System.out.println("Col: " + column + ", play: " + play + ", player: " + player);
+            if (FFX.Play (column, play, player))
+            {
+                System.out.print(FFX);
+                winner = FFX.Test();
+                player = player == 1 ? 2 : 1;
+            }
+            else
+                System.out.print("Invalid move - try again.\n");
+        }
+        if (winner == 1)
+            System.out.print("Player 1!! You win!!\n");
+        else if (winner == 2)
+            System.out.print("Player 2!! You win!!\n");
+        else if (winner == 3)
+            System.out.print("Sorry, the cat wins!\n");
+        else
+            System.out.print("Sorry, there is no winner :(\n");
     }
 }
